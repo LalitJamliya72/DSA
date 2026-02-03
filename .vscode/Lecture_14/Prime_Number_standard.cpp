c765282deb86d5dc7cafb1a53bf435af371af503
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-03 13:58:45 +0530

    Notes

---
+++ .vscode/Lecture_14/Prime_Number_standard.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    bool ans = false;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            ans = true;
+            break;
+        }
+    }
+    if(ans){
+        cout<<"Prime nahi hai";
+    }
+    else{
+        cout<<"Prime hai";
+    }
+   return 0;
+}
