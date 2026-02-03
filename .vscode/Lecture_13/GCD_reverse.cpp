fef84692c01f18b3ce4969f5284f8472852f730e
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-03 13:14:20 +0530

    Notes

--- .vscode/Lecture_13/GCD_reverse.cpp
+++ .vscode/Lecture_13/GCD_reverse.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    while(b!=0){
-        int t = a%b;
+    int a, b;
+    cin >> a >> b;
+    while (b != 0)
+    {
+        int t = a % b;
         a = b;
         b = t;
     }
-    cout<<a<<endl;
-   return 0;
+    cout << a << endl;
+    return 0;
 }
