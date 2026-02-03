24cd9180dcc91366fd8df12058aaf918a9e678ed
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-03 14:41:45 +0530

    Notes

---
+++ .vscode/Lecture_14/Sum_of_digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum = 0;
+    while(n>0){
+        int d = n%10;
+        sum = sum+d;
+        n/=10;
+    }
+    cout<<abs(sum);
+   return 0;
+}
