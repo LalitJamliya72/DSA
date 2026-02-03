39ec9aa8d847ef5553ef0b37fbee876b48abbb5c
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-03 13:12:32 +0530

    Notes

--- .vscode/Lecture_13/GCD_stl.cpp
+++ .vscode/Lecture_13/GCD_stl.cpp
@@ -1,16 +1,15 @@
-#include<iostream>
-#include<algorithm>
-#include<climits>
+#include <iostream>
+#include <algorithm>
+#include <climits>
 using namespace std;
 int main()
 {
-    
     int c = INT_MAX;
     int d = INT_MIN;
     int a = 5;
     int b = 10;
-    cout<<__gcd(a,b)<<endl;
-    cout<<c;
-    cout<<d;
-   return 0;
+    cout << __gcd(a, b) << endl;
+    cout << c;
+    cout << d;
+    return 0;
 }
