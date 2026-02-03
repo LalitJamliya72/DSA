5847388094a856540d579b39871da0030eb6cbd5
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-03 13:15:19 +0530

    Notes

--- .vscode/Lecture_13/GCD.cpp
+++ .vscode/Lecture_13/GCD.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int ans = 1;
-    int a,b;
-    cin>>a>>b;
+    int a, b;
+    cin >> a >> b;
 
-    for(int i=1;i<=min(a,b);i++){
-        if(a%i==0 and b%i==0){
+    for (int i = 1; i <= min(a, b); i++)
+    {
+        if (a % i == 0 and b % i == 0)
+        {
             ans = i;
         }
     }
-    cout<<ans<<endl;
-   return 0;
+    cout << ans << endl;
+    return 0;
 }
