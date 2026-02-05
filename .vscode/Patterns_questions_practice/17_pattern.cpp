383897554f5d33e916b5c2d380141183dee22c36
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-05 15:00:38 +0530

    Notes

--- .vscode/Patterns_questions_practice/17_pattern.cpp
+++ .vscode/Patterns_questions_practice/17_pattern.cpp
@@ -1,40 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int m;
-    cin>>m;
-    int n = (m+1)/2;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    cin >> m;
+    int n = (m + 1) / 2;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
         }
-        for(int j=i-1;j>=1;j--){
-            cout<<"* ";
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << "* ";
         }
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    for(int i=n-1;i>=1;i--){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
         }
-        for(int j=i-1;j>=1;j--){
-            cout<<"* ";
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << "* ";
         }
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
-   return 0;
+    return 0;
 }
