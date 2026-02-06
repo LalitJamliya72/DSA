370fbecef13f00f443f4f2028483b249e95f3f28
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-06 14:54:30 +0530

    Notes

--- .vscode/Patterns_questions_practice/6_pattern.cpp
+++ .vscode/Patterns_questions_practice/6_pattern.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=2*i-2;j++){
-            cout<<"  ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 2 * i - 2; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=n-i+1;j++){
-            cout<<"* ";
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-   return 0;
+    return 0;
 }
