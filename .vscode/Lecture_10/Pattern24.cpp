84f293bd72c744f154b3853722a25927d011e41e
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-06 15:52:01 +0530

    Notes

--- .vscode/Lecture_10/Pattern24.cpp
+++ .vscode/Lecture_10/Pattern24.cpp
@@ -1,28 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-          cout<<"  ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=n-i+1;j++){
-            cout<<"* ";
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-     for(int i=n-1;i>=1;i--){
-        for(int j=1;j<=n-i;j++){
-          cout<<"  ";
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=n-i+1;j++){
-            cout<<"* ";
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-   
 
-   return 0;
+    return 0;
 }
