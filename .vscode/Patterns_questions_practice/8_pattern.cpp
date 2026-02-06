8d3318cb963f07293edbf947614f96092d87528a
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-06 14:54:01 +0530

    Notes

--- .vscode/Patterns_questions_practice/8_pattern.cpp
+++ .vscode/Patterns_questions_practice/8_pattern.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if(j==i || j==n-i+1){
-                cout<<"* ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j == i || j == n - i + 1)
+            {
+                cout << "* ";
             }
-            else{
-                cout<<"  ";
+            else
+            {
+                cout << "  ";
             }
         }
-        cout<<endl;
+        cout << endl;
     }
-   return 0;
+    return 0;
 }
