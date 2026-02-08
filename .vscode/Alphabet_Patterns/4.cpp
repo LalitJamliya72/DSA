b7831699a34e8c4fb07154bae7367d6acab1818c
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-08 10:42:52 +0530

    Notes

--- .vscode/Alphabet_Patterns/4.cpp
+++ .vscode/Alphabet_Patterns/4.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
         int a = 1;
-        for(int j=1;j<=i;j++){
-        int d = a+68;
-        char ch = (char)d;
-        cout<<ch<<" "; 
-        a--;
+        for (int j = 1; j <= i; j++)
+        {
+            int d = a + 68;
+            char ch = (char)d;
+            cout << ch << " ";
+            a--;
         }
-        cout<<endl;
+        cout << endl;
     }
 
-   return 0;
+    return 0;
 }
