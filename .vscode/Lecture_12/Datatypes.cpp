00be411df0bd232f405e5f4761c9b88470b03b93
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-09 00:06:46 +0530

    Notes

--- .vscode/Lecture_12/Datatypes.cpp
+++ .vscode/Lecture_12/Datatypes.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    float a= 9.9356789654339383834;
-    cout<<a*4.5<<endl;
-   return 0;
+    float a = 9.9356789654339383834;
+    cout << a * 4.5 << endl;
+    return 0;
 }
