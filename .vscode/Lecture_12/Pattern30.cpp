7996003c1e9f4a6c0726c36f93beadc7aa33f736
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-09 00:07:31 +0530

    Notes

--- .vscode/Lecture_12/Pattern30.cpp
+++ .vscode/Lecture_12/Pattern30.cpp
@@ -1,25 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    int m = (n+1)/2;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-             if(i>=2 && i<=n-1 && j>=2 && j<=n-1){
-                if(i==m && j==m) {
-                    cout<<"@ ";
-             }
-             else{
-                cout<<"# ";
+    cin >> n;
+    int m = (n + 1) / 2;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i >= 2 && i <= n - 1 && j >= 2 && j <= n - 1)
+            {
+                if (i == m && j == m)
+                {
+                    cout << "@ ";
+                }
+                else
+                {
+                    cout << "# ";
                 }
             }
-             else{
-                cout<<"* ";
+            else
+            {
+                cout << "* ";
             }
         }
-         cout<<endl;
+        cout << endl;
     }
-   return 0;
+    return 0;
 }
