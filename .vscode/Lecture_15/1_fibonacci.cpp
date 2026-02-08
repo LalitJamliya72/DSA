64bb56b1ad6c1d5d0509ae76ae4c764643e253f9
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-09 00:11:38 +0530

    Notes

--- .vscode/Lecture_15/1_fibonacci.cpp
+++ .vscode/Lecture_15/1_fibonacci.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fibbo(int g){
-      if(g==0 or g==1){
-        return g;
-      }
-      return fibbo(g-1)+fibbo(g-2);
+int fibbo(int g)
+{
+  if (g == 0 or g == 1)
+  {
+    return g;
+  }
+  return fibbo(g - 1) + fibbo(g - 2);
 }
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<fibbo(n)<<endl;
-   return 0;
+  int n;
+  cin >> n;
+  cout << fibbo(n) << endl;
+  return 0;
 }
