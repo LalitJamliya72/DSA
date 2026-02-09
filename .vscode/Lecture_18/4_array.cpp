53ec90fe0d20839740b600b9b7ebdd704aaafa7d
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-10 00:44:22 +0530

    Notes

--- .vscode/Lecture_18/4_array.cpp
+++ .vscode/Lecture_18/4_array.cpp
@@ -1,19 +1,20 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-     int ans = 0;
-     for(int i=0;i<n;i++){
-        ans+=arr[i];
-        
-     }
-    cout<<ans<<endl;
-   return 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += arr[i];
+    }
+    cout << ans << endl;
+    return 0;
 }
