a2dd119922863214743c94d3eb906855e4b340d0
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-10 00:35:48 +0530

    Notes

--- .vscode/Lecture_18/practice.cpp
+++ .vscode/Lecture_18/practice.cpp
@@ -1,18 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-	int n;
-	cin>>n;
-	int d;
-    cin>>d;
+int main()
+{
+    int n;
+    cin >> n;
+    int d;
+    cin >> d;
     int count = 0;
-    while(n>0){
-        if(n%10==d){
+    while (n > 0)
+    {
+        if (n % 10 == d)
+        {
             count++;
-            n/=10;
+            n /= 10;
         }
     }
-    cout<<count;
-	
-	return 0;
+    cout << count;
+
+    return 0;
 }
