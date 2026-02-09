a48ce64da702c0fd2f0dc7f1bb168f041af49969
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-10 00:44:46 +0530

    Notes

--- .vscode/Lecture_18/3_array.cpp
+++ .vscode/Lecture_18/3_array.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-     int ans = INT_MAX;
-     for(int i=0;i<n;i++){
-        if(ans>arr[i]){
-        ans = arr[i];
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (ans > arr[i])
+        {
+            ans = arr[i];
         }
-     }
-    cout<<ans<<endl;
-   return 0;
+    }
+    cout << ans << endl;
+    return 0;
 }
