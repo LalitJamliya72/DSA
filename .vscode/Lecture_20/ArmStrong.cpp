3c0083cac64db8b733d7c8a6fb2d2ac01f65e540
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-11 15:54:14 +0530

    Notes

--- .vscode/Lecture_20/ArmStrong.cpp
+++ .vscode/Lecture_20/ArmStrong.cpp
@@ -1,29 +1,33 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
-      int n;
-      cin>>n;
-      int og = n;
-      int temp = n;
-       int sum = 0;
-      int l = 0;
-      while(temp!=0){
-        temp = temp/10;
+    int n;
+    cin >> n;
+    int og = n;
+    int temp = n;
+    int sum = 0;
+    int l = 0;
+    while (temp != 0)
+    {
+        temp = temp / 10;
         l++;
-      }
-      while(n!=0){
-        int k = n%10;
-        sum = sum+pow(k,l);
-        n/=10;
-      }
-      if(og==sum){
-        cout<<"Armstrong";
-      }
-      else{
-        cout<<"Not Armstrong";
-      }
+    }
+    while (n != 0)
+    {
+        int k = n % 10;
+        sum = sum + pow(k, l);
+        n /= 10;
+    }
+    if (og == sum)
+    {
+        cout << "Armstrong";
+    }
+    else
+    {
+        cout << "Not Armstrong";
+    }
 
-   return 0;
+    return 0;
 }
