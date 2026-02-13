17ad40fef07b699d1219cf940934011777533ac3
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-13 21:10:05 +0530

    Notes

--- .vscode/Assignment_questions/16.cpp
+++ .vscode/Assignment_questions/16.cpp
@@ -1,27 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int m;
-    cin>>m;
-    int n = (m+1)/2;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    cin >> m;
+    int n = (m + 1) / 2;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=2*i-1;j++){
-            cout<<"* ";
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    for(int i=n-1;i>=1;i--){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=2*i-1;j++){
-            cout<<"* ";
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-   return 0;
+    return 0;
 }
