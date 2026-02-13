abba6c20fc26a8f99e2ad1f4b928da993e3aa512
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-13 21:09:44 +0530

    Notes

--- .vscode/Assignment_questions/15.cpp
+++ .vscode/Assignment_questions/15.cpp
@@ -1,20 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=i;j++){
-            cout<<j+i-1<<" ";
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j + i - 1 << " ";
         }
-        for(int j=i-1;j>=1;j--){
-            cout<<j+i-1<<" ";
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << j + i - 1 << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-   return 0;
+    return 0;
 }
