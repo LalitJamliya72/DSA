5250c15821e7a517b9f929b2c3d956a2c9fd9e4e
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-13 21:11:12 +0530

    Notes

--- .vscode/Assignment_questions/19.cpp
+++ .vscode/Assignment_questions/19.cpp
@@ -1,32 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=i;j++){
-            cout<<j+i-1<<" ";
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j + i - 1 << " ";
         }
-        for(int j=i-1;j>=1;j--){
-            cout<<j+i-1<<" ";
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << j + i - 1 << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    for(int i=n-1;i>=1;i--){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=1;j<=i;j++){
-            cout<<j+i-1<<" ";
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j + i - 1 << " ";
         }
-        for(int j=i-1;j>=1;j--){
-            cout<<j+i-1<<" ";
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << j + i - 1 << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-   return 0;
+    return 0;
 }
