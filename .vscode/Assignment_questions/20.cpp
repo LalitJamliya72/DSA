8e30e67fc0eed078824262be653e0795ba379963
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-13 21:11:51 +0530

    Notes

--- .vscode/Assignment_questions/20.cpp
+++ .vscode/Assignment_questions/20.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            if(j==1 || j==i){
-            cout<<i<<" ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (j == 1 || j == i)
+            {
+                cout << i << " ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
         }
-        else{
-            cout<<"0 ";
-        }
+        cout << endl;
     }
-        cout<<endl;
-    }
-   return 0;
+    return 0;
 }
