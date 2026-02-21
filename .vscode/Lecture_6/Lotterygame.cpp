d4cdcac84397f353dc4d06f97391ece04068f663
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-21 13:27:04 +0530

    Notes

--- .vscode/Lecture_6/Lotterygame.cpp
+++ .vscode/Lecture_6/Lotterygame.cpp
@@ -1,23 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    if(n>=300 && n<=460){
-        cout<<"Macbook";
+    cin >> n;
+    if (n >= 300 && n <= 460)
+    {
+        cout << "Macbook";
     }
-    else if(n>=200 && n<=280){
-        cout<<"Kurkure";
+    else if (n >= 200 && n <= 280)
+    {
+        cout << "Kurkure";
     }
-    else if(n>=1100 && n<=1500){
-        cout<<"Cycle";
+    else if (n >= 1100 && n <= 1500)
+    {
+        cout << "Cycle";
     }
-    else if(n>50 && n<=80){
-        cout<<"Bike";
+    else if (n > 50 && n <= 80)
+    {
+        cout << "Bike";
     }
-    else{
-        cout<<"Better luck next time";
+    else
+    {
+        cout << "Better luck next time";
     }
-   return 0;
+    return 0;
 }
