55a6b70f24888ac71a9d76a4e6e18e5bf4b87821
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-24 13:02:51 +0530

    Notes

---
+++ .vscode/Assignment_2/Grade_Card_Program.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cin>>marks;
+    if(marks>=90){
+        cout<<"Grade A";
+    }
+    else if(marks>=75){
+        cout<<"Grade B";
+    }
+    else{
+        cout<<"Grade C";
+    }
+   return 0;
+}
