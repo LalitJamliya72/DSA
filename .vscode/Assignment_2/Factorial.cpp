4e79d0ae2787133abafa6e4b4b690b12034bf6db
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-25 14:04:44 +0530

    Notes

--- .vscode/Assignment_2/Factorial.cpp
+++ .vscode/Assignment_2/Factorial.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int factorial = 1;
-    for(int i=1;i<=n;i++){
-        factorial=factorial*i;
-        cout<<factorial<<endl;
-
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+        cout << factorial << endl;
     }
 
-   return 0;
+    return 0;
 }
