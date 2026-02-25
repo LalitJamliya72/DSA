8a29015b427523bb5ca54c0eb009c9a8401d034e
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-25 14:05:25 +0530

    Notes

--- .vscode/Assignment_2/Maximum_Of_Three_Numbers.cpp
+++ .vscode/Assignment_2/Maximum_Of_Three_Numbers.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a>b && a>c){
-        cout<<"a is largest";
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b && a > c)
+    {
+        cout << "a is largest";
     }
-    else if(b>c){
-        cout<<"b is largest";
+    else if (b > c)
+    {
+        cout << "b is largest";
     }
-    else{
-        cout<<"c is largest";
+    else
+    {
+        cout << "c is largest";
     }
-   return 0;
+    return 0;
 }
