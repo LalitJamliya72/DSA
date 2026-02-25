beb5b26ffc92c22b1d315731ae820933f67d8009
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-25 14:04:23 +0530

    Notes

--- .vscode/Assignment_2/Even_numbers.cpp
+++ .vscode/Assignment_2/Even_numbers.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        if(i%2==0){
-            cout<<i<<endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
         }
     }
-   return 0;
+    return 0;
 }
