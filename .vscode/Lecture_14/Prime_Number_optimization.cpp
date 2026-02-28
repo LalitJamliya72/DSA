295279e49c8b4bfa74da2b6c31f6c67c81221b83
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-28 11:37:17 +0530

    Notes

--- .vscode/Lecture_14/Prime_Number_optimization.cpp
+++ .vscode/Lecture_14/Prime_Number_optimization.cpp
@@ -1,22 +1,26 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     bool ans = false;
-    for(int i=2;i<=sqrt(n);i++){
-        if(n%i==0){
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
             ans = true;
             break;
         }
     }
-    if(ans){
-        cout<<"Prime nahi hai";
+    if (ans)
+    {
+        cout << "Prime nahi hai";
     }
-    else{
-        cout<<"Prime hai";
+    else
+    {
+        cout << "Prime hai";
     }
-   return 0;
+    return 0;
 }
