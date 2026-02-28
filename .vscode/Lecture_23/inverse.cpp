f543ec70226f2159f768789ced7ad16e4a027157
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-03-01 00:11:09 +0530

    Notes

--- .vscode/Lecture_23/inverse.cpp
+++ .vscode/Lecture_23/inverse.cpp
@@ -1,20 +1,19 @@
-#include<iostream>
+#include <iostream>
 #include<vector>
 using namespace std;
-vector<int> inverse(int n){
+vector<int> convert(int n){
     vector<int> num;
     while(n!=0){
-        int k = n%10;
+        int k=n%10;
         num.push_back(k);
-        n/=10;
+        n=n/10;
     }
     return num;
 }
-int main()
-{
+int main() {
     int n;
     cin>>n;
-    vector<int> num=inverse(n);
+    vector<int> num=convert(n);
     vector<int> ans(num.size()+1);
     for(int i=0;i<num.size();i++){
         ans[num[i]]=i+1;
@@ -22,5 +21,5 @@ int main()
     for(int i=num.size();i>=1;i--){
         cout<<ans[i];
     }
-   return 0;
+    return 0;
 }
