3905913e266ba61624e9ac0195cb11be84912772
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-02-28 11:38:43 +0530

    Notes

--- .vscode/Lecture_14/Reverse_digit.cpp
+++ .vscode/Lecture_14/Reverse_digit.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-   int n;
-   cin>>n;
-   int rev = 0;
-    while(n>0){
-        int d = n%10;
-        rev = rev*10+d;
-        n/=10;
+    int n;
+    cin >> n;
+    int rev = 0;
+    while (n > 0)
+    {
+        int d = n % 10;
+        rev = rev * 10 + d;
+        n /= 10;
     }
-    cout<<rev;
-   return 0;
+    cout << rev;
+    return 0;
 }
