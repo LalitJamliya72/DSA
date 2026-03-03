7e8f902598f473add1a434383f6fc91bc6d3e967
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-03-03 12:21:39 +0530

    Notes

--- .vscode/Hackerblocks/inverse_array.cpp
+++ .vscode/Hackerblocks/inverse_array.cpp
@@ -1,34 +1,28 @@
 #include <iostream>
-#include <vector>
+#include<vector>
 using namespace std;
-vector<int> convert(int n)
-{
+vector<int> convert(int n){
     vector<int> number;
-    while (n != 0)
-    {
-        int k = n % 10;
+    while(n!=0){
+        int k=n%10;
         number.push_back(k);
-        n = n / 10;
+        n=n/10;
     }
     return number;
 }
-int main()
-{
+int main() {
     int n;
-    cin >> n;
-    vector<int> num = convert(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> num[i];
+    cin>>n;
+    vector<int> num=convert(n);
+    for(int i=0;i<n;i++){
+        cin>>num[i];
     }
-    vector<int> ans(num.size() + 1);
-    for (int i = 0; i < num.size(); i++)
-    {
-        ans[num[i]] = i + 1;
+    vector<int> ans(num.size()+1);
+    for(int i=0;i<num.size();i++){
+        ans[num[i]]=i+1;
     }
-    for (int i = num.size(); i >= 1; i--)
-    {
-        cout << ans[i];
+    for(int i=num.size();i>=1;i--){
+        cout<<ans[i];
     }
     return 0;
 }
