cb13c1d0e3d7fe6bd2e9b19bc3e1c611429713ad
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-03-07 16:02:45 +0530

    Notes

--- .vscode/Assignment_2/Simple_interest_calculation.cpp
+++ .vscode/Assignment_2/Simple_interest_calculation.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int p,r,t,si;
-    cin>>p>>r>>t;
-    si = (p*r*t)/100;
-    cout<<si;
-   return 0;
+    int p, r, t, si;
+    cin >> p >> r >> t;
+    si = (p * r * t) / 100;
+    cout << si;
+    return 0;
 }
