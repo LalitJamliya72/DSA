dffe5301ea513d45b5a121147e677ba2855019d4
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-03-07 16:03:07 +0530

    Notes

--- .vscode/Assignment_2/Sum_Of_digits.cpp
+++ .vscode/Assignment_2/Sum_Of_digits.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int sum = 0;
-    while(n>0){
-        sum+=n%10;
-        n = n/10;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n = n / 10;
     }
-    cout<<sum<<endl;
-   return 0;
+    cout << sum << endl;
+    return 0;
 }
