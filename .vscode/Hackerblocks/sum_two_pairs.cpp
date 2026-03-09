8f15d76b87c649d72d238cad8d8f6d583f07ce68
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-03-09 12:32:59 +0530

    Notes

---
+++ .vscode/Hackerblocks/sum_two_pairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[1000];
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int target;
+    cin >> target;
+
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(a[i] + a[j] == target) {
+                cout << a[i] << " and " << a[j] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
