b998493b96c16b2f048941df422d605b69ad6937
LalitJamliya72 <Lalitjamliya72@gmail.com>
2026-03-20 15:51:55 +0530

    Notes

---
+++ Arrays/Array_target_sum_triplets.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+        }
+    }
+    }
+    int target;
+    cin>>target;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(arr[i]+arr[j]+arr[k]==target){
+                    cout<<arr[i]<<", "<<arr[j]<<"and"<<arr[k]<<endl;
+                }
+            }
+        }
+    }
+   return 0;
+}
